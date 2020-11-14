fdbbabd8ffcf4233597aca165fa3055808059471
Benjamin Prats <prats.benjamin@gmail.com>
2020-11-14 13:32:26 -0500

    New terrain animation, controller improvement...

--- MixJam2020/Assets/Scripts/CarController.cs
+++ MixJam2020/Assets/Scripts/CarController.cs
@@ -6,37 +6,55 @@ public class CarController : MonoBehaviour
 {
     public Rigidbody _rb;
     public Transform _cameraTarget;
-    public Transform _rayCheckPoint;
+    public Transform _rayGroundCheckPoint;
+    public Transform _raySlopeCheckPoint;
     public LayerMask _groundMask;
     public BoostBar _boostBar;
 
+    [Space(10)]
+    public float _accelerationFactor = 10.0f;
     public float _forwardAcceleration = 10.0f;
     public float _reverseAcceleration = 5.0f;
 
+    [Space(10)]
     public float _boostFactor = 1.0f;
+    public float _boostFactorInAir = 1.0f;
     public float _boostDuration = 5.0f; // if 5 means take 5 seconds to empty it fully 
     public float _boostCameraDelay = 5.0f;
 
+    [Space(10)]
     public float _rotationStrength = 5.0f;
     public float _cameraRotationRadius = 10.0f;
     public float _cameraSmoothTime = 0.3f;
 
-    public float _smoothNormalRotation = 10.0f;
+    [Space(10)]
+    public float _smoothNormalRotationMax = 10.0f;
+    public float _lengthNormalRotationCheck = 5.0f;
     //    public float _maxSpeed = 10.0f;
 
+    [Space(10)]
     public float _gravityStrength = 10.0f;
     public float _groundRayCheckLength = 0.5f;
+    public float _dragGround = 1.5f;
+    public float _dragAir = 0.5f;
+
+    [Space(10)]
+    [Range(0.0f, 90.0f)]
+    public float _maxSlope = 70.0f;
+
 
     private float _accelerationInput;
     private float _rotationInput;
+    private float _verticalInput;
 
     private bool _isGrounded = false;
-    private bool _isBoostActived = false;
+    private bool _isBoostActivated = false;
 
     private float _boostLevel = 0.0f; // 0 to 1
 
-
     private Vector3 _cameraFollowVelocity = Vector3.zero;
+
+    private Quaternion _futureRotation;
 
     // Start is called before the first frame update
     void Start()
@@ -48,30 +66,19 @@ public class CarController : MonoBehaviour
     void Update()
     {
         // Inputs
-        float verticalInput = Input.GetAxis("Vertical");
+        _verticalInput = Input.GetAxis("Vertical");
         _rotationInput = Input.GetAxis("Horizontal");
 
-        _accelerationInput = verticalInput > 0.0f ? verticalInput * _forwardAcceleration :
-                             verticalInput < 0.0f ? verticalInput * _reverseAcceleration : 0.0f;
+        _accelerationInput = _verticalInput > 0.0f ? _verticalInput * _forwardAcceleration :
+                             _verticalInput < 0.0f ? _verticalInput * _reverseAcceleration : 0.0f;
 
         _boostBar.SetSlider(_boostLevel);
-        _isBoostActived = (Input.GetButton("Jump") && _boostLevel > 0.0f) ? true : false;
-
-        //if ()
-        //{
-
-        //    _accelerationInput += (_forwardAcceleration * _boostFactor);
-        //    _rb.drag = 0.0f;
-        //}
-        //else
-        //{
-        //    _rb.drag = 0.5f;
-        //}
+        _isBoostActivated = (Input.GetButton("Jump") && _boostLevel > 0.0f) ? true : false;
 
         // Camera movement
-        Vector3 cameraTargetTarget = new Vector3(_rotationInput * _cameraRotationRadius, 0.0f, 0.0f);
+        Vector3 cameraTargetTarget = new Vector3(_rotationInput * _cameraRotationRadius, _cameraTarget.localPosition.y, 0.0f);
         float cameraSmoothTime = _cameraSmoothTime;
-        if (_isBoostActived)
+        if (_isBoostActivated)
         {
             cameraTargetTarget.z = -_boostCameraDelay;
             cameraSmoothTime = 0.5f;
@@ -79,34 +86,53 @@ public class CarController : MonoBehaviour
         _cameraTarget.localPosition = Vector3.SmoothDamp(_cameraTarget.localPosition, cameraTargetTarget, ref _cameraFollowVelocity, _cameraSmoothTime);
 
         // Update car transform
-        if (_isGrounded || _isBoostActived)
+//        UpdateCarRotation();
+        if (_isGrounded || _isBoostActivated)
         {
-            float verticalFactor = verticalInput < 0.01f && (_isBoostActived || _rb.velocity.sqrMagnitude > 1.0f) ? 1.0f : verticalInput; 
+            float verticalFactor = _verticalInput == 0.0f && (_isBoostActivated || (_isGrounded && _rb.velocity.sqrMagnitude > 1.0f)) ? Mathf.Clamp01(_rb.velocity.sqrMagnitude) : _verticalInput;
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0.0f, _rotationInput * _rotationStrength * Time.deltaTime * verticalFactor, 0.0f));
         }
-
 
         transform.position = _rb.transform.position;
     }
 
     private void FixedUpdate()
     {
-        _isGrounded = false;
-
+        bool isGroundedOld = _isGrounded;
         RaycastHit hit;
-        if (Physics.Raycast(_rayCheckPoint.position, -transform.up, out hit, _groundRayCheckLength, _groundMask))
+        if (Physics.Raycast(_rayGroundCheckPoint.position, -transform.up, out hit, _groundRayCheckLength, _groundMask))
         {
-            // car perpendicular to road
-            Quaternion targetRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _smoothNormalRotation);
+            //// car perpendicular to road
+            //Quaternion targetRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _smoothNormalRotation);
 
             _isGrounded = true;
+        }
+        else
+        {
+            _isGrounded = false;
+        }
+
+        UpdateCarRotation();
+
+        if (isGroundedOld != _isGrounded)
+        {
+            _rb.drag = _isGrounded ? _dragGround : _dragAir;
         }
 
         if (_isGrounded)
         {
             if (_accelerationInput != 0.0f)
-                _rb.AddForce(transform.forward * _accelerationInput);
+                _rb.AddForce(transform.forward * _accelerationInput * _accelerationFactor);
+
+            //float dot = Vector3.Dot(transform.up, Vector3.up);
+            //if (dot < 0.7f)
+            //{
+            //    float slopeDecelerationFactor = Mathf.Clamp(1.0f / (dot * dot), 0.0f, 20.0f) / 10.0f;
+            //    Debug.Log("Deceleration factor: " + slopeDecelerationFactor);
+            //    _rb.AddForce(-transform.forward * _accelerationFactor * slopeDecelerationFactor * _accelerationFactor);
+            //}
+
         }
         else
         {
@@ -114,13 +140,29 @@ public class CarController : MonoBehaviour
                 _rb.AddForce(Vector3.down * _gravityStrength);
         }
 
-        if (_isBoostActived)
+        if (_isBoostActivated)
         {
+            float boostFactor = _isGrounded ? _boostFactor : _boostFactorInAir;
+            _rb.AddForce(transform.forward * boostFactor * _accelerationFactor);
             _boostLevel -= Time.fixedDeltaTime * 1.0f / _boostDuration;
             if (_boostLevel < 0.0f)
                 _boostLevel = 0.0f;
-            _rb.AddForce(transform.forward * _boostFactor);
         }
+    }
+
+    private void UpdateCarRotation()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(_rayGroundCheckPoint.position, -transform.up, out hit, _lengthNormalRotationCheck, _groundMask))
+        {
+            float smoothnessFactor = 1.0f / (hit.distance / _lengthNormalRotationCheck);
+            if (smoothnessFactor > _smoothNormalRotationMax)
+                smoothnessFactor = _smoothNormalRotationMax;
+            // car perpendicular to road
+            Quaternion targetRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, smoothnessFactor);
+        }
+
     }
 
     public void ReloadBoost(float amount)
@@ -130,10 +172,20 @@ public class CarController : MonoBehaviour
             _boostLevel = 1.0f;
     }
 
+    public float GetVerticalInput()
+    {
+        return _verticalInput;
+    }
+
+    public float GetRotationInput()
+    {
+        return _rotationInput;
+    }
+
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(_rayCheckPoint.position, _rayCheckPoint.position - (transform.up * _groundRayCheckLength));
+        Gizmos.DrawLine(_rayGroundCheckPoint.position, _rayGroundCheckPoint.position - (transform.up * _groundRayCheckLength));
     }
 }
